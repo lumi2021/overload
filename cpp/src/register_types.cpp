aa275fb73ff6afd9b67630f90c795b95141d3d1a
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2026-06-06 15:59:21 -0300

    second commit lol

---
+++ cpp/src/register_types.cpp
@@ -0,0 +1,48 @@
+#include <godot_cpp/godot.hpp>
+#include <godot_cpp/core/class_db.hpp>
+
+#include "nodes/gdexample.h"
+
+using namespace godot;
+
+void initialize_module(
+    ModuleInitializationLevel p_level
+) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+
+    ClassDB::register_class<GDExample>();
+}
+
+void uninitialize_module(
+    ModuleInitializationLevel p_level
+) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+}
+
+extern "C" {
+
+GDExtensionBool GDE_EXPORT __overloaded_core_init__(
+    GDExtensionInterfaceGetProcAddress p_get_proc_address,
+    GDExtensionClassLibraryPtr p_library,
+    GDExtensionInitialization *r_initialization
+) {
+    godot::GDExtensionBinding::InitObject init_obj(
+        p_get_proc_address,
+        p_library,
+        r_initialization
+    );
+
+    init_obj.register_initializer(initialize_module);
+    init_obj.register_terminator(uninitialize_module);
+    init_obj.set_minimum_library_initialization_level(
+        MODULE_INITIALIZATION_LEVEL_SCENE
+    );
+
+    return init_obj.init();
+}
+
+}
